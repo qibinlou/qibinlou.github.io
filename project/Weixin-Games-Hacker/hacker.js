9a0b29a6d4b7aca9410ea83e22d248ad76d5d0d1
Leo Lou <lqb12345@126.com>
2014-08-01 20:48:11 +0800

    Create Weixin Games Hacker

---
+++ project/Weixin-Games-Hacker/hacker.js
@@ -0,0 +1,11 @@
+(function(window, document, undefined){
+    window.$ = function(selector) {
+        return document.getElementById(selector);
+    }
+    
+    
+    $(document).ready(function() {
+        
+    });
+
+}(window, document, undefined));
